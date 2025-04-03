c62b2abaa97a9d1cdd86223b5ae3418635261727
GabrielSouzaSi <gos.gabri@gmail.com>
2025-04-02 20:45:01 -0400

    feat: add Icon and Text in Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,23 @@
-import { View, ViewProps, Image, ImageProps } from "react-native";
-import { Button } from "./button";
-import { Card } from "./card";
-import { Img } from "./img";
+import { View, ViewProps, Text } from "react-native"
+import { Button } from "./button"
+import { Card } from "./card"
+import { Img } from "./img"
+import { Icon } from "./icon"
 
-type HeaderProps = ViewProps;
+type HeaderProps = ViewProps
 
 function Header({ children, className, ...rest }: HeaderProps) {
-
-    return (
-        <View
-            className={className}
-            {...rest}
-        >
-            {children}
-        </View>
-    )
+	return (
+		<View className={className} {...rest}>
+			{children}
+		</View>
+	)
 }
-
-
 
 Header.Img = Img
 Header.Button = Button
 Header.Card = Card
+Header.Icon = Icon
+Header.Text = Text
 
 export { Header }
