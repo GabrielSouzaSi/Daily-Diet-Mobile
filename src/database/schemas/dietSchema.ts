36ee1f56b1f533af7de08a35dc2722f71659a0f5
GabrielSouzaSi <gos.gabri@gmail.com>
2025-04-05 13:25:15 -0400

    formated

--- src/database/schemas/dietSchema.ts
+++ src/database/schemas/dietSchema.ts
@@ -1,13 +1,13 @@
 // comando para gerar a tabela
 // npx drizzle-kit generate
 
-import { integer, sqliteTable, text} from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const diet = sqliteTable("diets", {
-    id: integer("id").primaryKey(),
-    name: text("name"),
-    description: text("description"),
-    date: text("date"),
-    time: text("time"),
-    compliant: integer("compliant", {mode: "boolean"}),
+	id: integer("id").primaryKey(),
+	name: text("name"),
+	description: text("description"),
+	date: text("date"),
+	time: text("time"),
+	compliant: integer("compliant", { mode: "boolean" }),
 })
